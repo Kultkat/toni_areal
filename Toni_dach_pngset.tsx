40e572c1328b280f9e02bd7498abc0691f10a66b
Kaspar <Kultkat@users.noreply.github.com>
2021-03-08 21:22:31 +0100

    Add files via upload

---
+++ Toni_dach_pngset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tisch" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="tisch.png" width="32" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="playAudio" value="Ambiente_dach.mp3"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ toni_dachpng_set.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="toni_dachpng_set" tilewidth="32" tileheight="32" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="tile_file.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="grau_quadrat.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="gras_.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="gras_2.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="graugummi.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="grobgras.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="gulli_deckel.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="innehof.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="gross_blatt.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="stuhl.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="gruen.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="glas_innehof.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="gras_braun.png"/>
+ </tile>
+</tileset>
